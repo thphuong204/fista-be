a3307d4f8d71ded739da78a72da0925328cc4378
Phuong Tran <thphuong204@gmail.com>
2022-12-21 19:36:28 +0700

    Add error handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { sendResponse, AppError } =require("./helpers/utils.js")
 require(
     "dotenv"
 ).config()
@@ -29,5 +30,24 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use('/', indexRouter);
+
+// catch 404 and forard to error handler
+app.use((req, res, next) => {
+    const err = new AppError(404,"Not Found","Bad Request");
+    next(err);
+});
+
+/* Initialize Error Handling */
+app.use((err, req, res, next) => {
+    console.log("ERROR", err);
+      return sendResponse(
+        res,
+        err.statusCode ? err.statusCode : 500,
+        false,
+        null,
+        { message: err.message },
+        err.isOperational ? err.errorType : "Internal Server Error"
+      );
+});
 
 module.exports = app;
