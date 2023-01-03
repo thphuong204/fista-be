4756a85371fa9702f6a151bf13ecbf4b996e40be
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-01-03 08:08:25 +0700

    continuously WIP user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,151 @@ const userController = {};
 const { sendResponse, AppError } = require("../helpers/utils");
 const users_role_array = ["user", "admin"];
 
-userController.createUser = async (req, res, next) => {
+// Create a new user
+userController.createUser = async ( req, res, next ) => {
     console.log("createUser");
+    try {
+        if (!req.body) throw new AppError(400, "No request body", "Bad Request");
+    
+        const createdUser = await User.create(req.body);
+        const noPasswordCreatedUser = await User.find({},{"password": 0})
+        console.log("createdUser", createdUser)
+        console.log("noPasswordCreatedUser", noPasswordCreatedUser)
+        
+        sendResponse(
+          res,
+          200,
+          true,
+          noPasswordCreatedUser,
+          null,
+          ""
+        );
+      } catch (err) {
+        next(err);
+      }
 };
+
+// Get all users
+userController.getUsers = async ( req, res, next ) => {
+
+    const filterKeyArr = ["name", "email"];
+    const filter = req.query;
+    const page_number = req.query.page || 1;
+    const page_size = req.query.limit || 10; 
+
+    const keywordArr = Object.keys(filter);
+    keywordArr.forEach((keyword) => {
+      if (!filterKeyArr.includes(keyword))
+        throw new AppError(400, `query ${keyword} is not accepted`, "Bad request");
+      if (!filter[keyword]) delete filter[keyword];
+    });
+
+    let total = await User.count(filter);
+    if (!total) {
+      throw new AppError (404,"User Not Found","Bad request")
+      return
+    }
+
+    //skip number
+    let offset = page_size * (page_number - 1);
+    const listOfUsers = await User.find(filter, {"password": 0}).skip(offset).limit(page_size);
+
+    let data = {total, page_size, page_number,  items: listOfUsers};
+
+    sendResponse(res, 200, true, data, null, "");
+};
+
+// Get user by id,
+userController.getUserById = async (req, res, next) => {
+    try {
+        if (!req.params._id)
+          throw new AppError(400, "User Id Not Found", "Bad Request");
+        const { _id } = req.params;
+    
+        const userById = await User.findById(_id, {"password": 0});
+        if (!userById || (userById.is_deleted.toString() === "true")) {
+          throw new AppError(400, "User Not Found", "Bad Request")
+          return
+        }
+        sendResponse(res, 200, true, userById, null, "");
+      } catch (error) {
+        next(error);
+      }
+};
+
+// Update user
+userController.updateUser = async (req, res, next) => {
+    try {
+        if (!req.body || !req.params._id) {
+            throw new AppError(400, "Requiring body and id to update user information", "Bad Request")
+            return
+        };
+        
+        const { _id } = req.params;
+        const bodyToUpdate = req.body;
+    
+        const userById = await User.findById(_id, {"password": 0});
+        if (!userById || (userById.is_deleted.toString() === "true")) {
+          throw new AppError(400, "User Not Found", "Bad Request")
+          return
+        }
+
+        const editKeyArr = ["name", "password"];
+        const keywordArray = Object.keys(bodyToUpdate);
+        keywordArray.forEach((keyword) => {
+            if (!editKeyArr.includes(keyword))
+              throw new AppError(400,`Keyword ${keyword} is not accepted. Only 'name' or 'password' are accepted for updating`,"Bad request");
+            if (!bodyToUpdate[keyword]) delete bodyToUpdate[keyword];
+        });
+
+        const options = { new: true };
+        const updatedUser = await User.findByIdAndUpdate(_id, bodyToUpdate, options).select({"password": 0});
+        if (!updatedUser) {
+            throw new AppError(404, "User Not Found", "Bad request")
+            return
+        };
+
+        sendResponse(res, 200, true, updatedUser, null, "");
+      } catch (error) {
+        next(error);
+      }
+};
+
+// Delete user
+userController.deleteUser = async (req, res, next) => {
+    try {
+        if (!req.params._id) {
+            throw new AppError(404, "User Not Found", "Bad request")
+            return
+        }
+        const { _id } = req.params;
+        const options = { new: true };
+
+        //only delete user not yet deleted
+        const idFoundCheck = await User.findById(_id)
+        if (!idFoundCheck || (idFoundCheck.is_deleted.toString() === "true")) {
+        throw new AppError(404,"User Not Found","Bad Request")
+        return
+        }
+
+        const deletedUser = await User.findByIdAndUpdate(
+            _id,
+            { is_deleted: true },
+            options
+        );
+
+        sendResponse(
+            res,
+            200,
+            true,
+            deletedUser,
+            null,
+            ""
+          );
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = userController;
--- routes/users.api.js
+++ routes/users.api.js
@@ -1,4 +1,58 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser
+} = require("../controllers/user.controller");
+
+// CREAT
+/** 
+    * @route POST /users
+    * @description Create a new user
+    * @body {name, email, password}
+    * @access Login required
+
+*/
+router.post("/", createUser);
+
+// READ
+// Get all users
+/**
+    * @route GET /users
+    * @description Get list of users
+    * @access Login required
+  */
+router.get("/", getUsers);
+
+// READ
+// Get user by id
+/**
+    * @route GET /users/:id
+    * @description Get information about user
+    * @access Login required
+  */
+router.get("/:_id", getUserById);
+
+// UPDATE
+// Update user's information
+/**
+    * @route GET /users/:id/?name=Phuong&password=123
+    * @description Update user's information
+    * @access Login required
+  */
+router.put("/:_id", updateUser);
+
+//DELETE
+/**
+    * @route DELETE /users/:id
+    * @description Delete a user
+    * @access Login required
+*/
+router.delete("/:_id", deleteUser);
+
+
 module.exports = router
