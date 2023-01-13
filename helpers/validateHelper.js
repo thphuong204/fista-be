7be91d43e743a035c039d67bad3ad86662d077a6
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-01-13 14:09:53 +0700

    Initialize validateHelper for checking 'query, body' of req

---
+++ helpers/validateHelper.js
@@ -0,0 +1,44 @@
+const validateHelper = {};
+const { AppError } = require("./utils");
+
+validateHelper.keywordQueryCheck = (query, acceptedFilterKeyArr) => {
+    const keywordQuerryArr = Object.keys(query);
+    // Remove page, limit out of query. These factors will be used directly when finding in collections
+    query.page = "";
+    query.limit = "";
+
+    keywordQuerryArr.forEach((keyword) => {
+        if (!acceptedFilterKeyArr.includes(keyword)) {
+            throw new AppError(
+                400,
+                `Query ${keyword} is not accepted`,
+                "Bad request"
+            );
+            return
+        }
+        if (!query[keyword]) delete query[keyword];
+    });
+
+    query.is_deleted = false;
+    return query;
+}
+
+validateHelper.keywordBodyCheck = (body, acceptedKeyArr) => {
+    const keywordBodyArr = Object.keys(body);
+    const acceptedKeyArrToText = acceptedKeyArr.join(", ")
+
+    keywordBodyArr.forEach((keyword) => {
+        if (!acceptedKeyArr.includes(keyword)) {
+            throw new AppError(
+                400,
+                `Keyword ${keyword} is not accepted. Only ${acceptedKeyArrToText} are accepted for updating`,
+                "Bad request"
+            );
+            return
+        }
+        if (!body[keyword]) delete body[keyword];
+    });
+    return body
+}
+
+module.exports = validateHelper;
